c2796f2cf3d69dc99705e636c0149c4ecffada49
suraj jaiswal <jaiswaljesus384@gmail.com>
2024-10-06 15:58:45 +0530

    URL changes

--- controller/indexController.js
+++ controller/indexController.js
@@ -2,7 +2,11 @@
 // Redirect the location after 5 sec
 
 function rederectLocation(){
-    window.location = "/view/home.html";
+    const pathArray = window.location.pathname.split('/');
+    const repositoryName = pathArray[1];
+    console.log(repositoryName);
+    
+    window.location = `/${repositoryName}/view/home.html`;
 }
 
 setTimeout(() => {
